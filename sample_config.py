44d8ebf27ec9cc662b6dcc9107f4db41d7c36d61
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-01-17 15:21:14 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,6 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = "5483813124:AAEPqJcVjFq3h7NG7rrcsGYPtGJKeDdAiE0"
-    API_ID = "9501538"
-   
-    API_HASH = "adb8864f52095ff4ca53e847a9250dac"
     
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5483813124:AAEPqJcVjFq3h7NG7rrcsGYPtGJKeDdAiE0")
 
